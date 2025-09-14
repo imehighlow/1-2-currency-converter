bfba755e4215e6956de11215f1f70c90fe81afe8
im64 <46765810+im64@users.noreply.github.com>
2025-09-14 13:39:10 +0300

    enter keypress event listener

--- exchange.js
+++ exchange.js
@@ -2,6 +2,7 @@ const apiKey = '78812af0de2b0012b38ba90f';
 const currencyApiRoot = `https://v6.exchangerate-api.com/v6/${apiKey}/latest`;
 const convertBtn = document.querySelector('#convertBtn')
 const output = document.querySelector('.output')
+const amountField = document.querySelector('#amount')
 
 async function getRates(currencyCode) {
     const url = `${currencyApiRoot}/${currencyCode}`;
@@ -15,7 +16,7 @@ async function convert() {
     console.log('convert function triggered')
     const from = document.querySelector('#from-currency').value.split(',')[0];
     const to = document.querySelector('#to-currency').value.split(',')[0];
-    const amount = document.querySelector('#amount').value;
+    const amount = amountField.value;
 
     const rates = await getRates(from);
     const value = rates[to] * amount;
@@ -23,4 +24,9 @@ async function convert() {
     output.textContent = value.toFixed(2);
 }
 
-convertBtn.addEventListener('click', convert)
+convertBtn.addEventListener('click', convert);
+amountField.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        convert();
+    }
+});
